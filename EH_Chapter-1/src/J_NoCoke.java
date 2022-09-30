ff2d0a0d07a0735826ca2593810f7cec680e9c8d
tuncaykozak <tuncaykozak@gmail.com>
2022-10-01 00:27:54 +0300

    Dr. Alban - No Coke

--- EH_Chapter-1/src/J_NoCoke.java
+++ EH_Chapter-1/src/J_NoCoke.java
@@ -16,6 +16,13 @@ public class J_NoCoke {
 
         // Dr. Alban - No Coke :
 
+        for(int i = 0; i < numberOfCanToDie; i++ ){
+            System.out.print(i);
+            System.out.print(" cans of coke and ");
+            System.out.print(((MG_OF_CAFFEINE_TO_DIE -(i * CAFFEINE_OF_12_OUNCE_COKE)) / CAFFEINE_OF_16_OUNCE_CUP_OF_COFFEE) + 1);
+            System.out.println(" cup of coffee can kill YOU!");
+        }
+
 
 
 
