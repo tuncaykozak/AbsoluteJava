d234e769b0372ac6411c689af322e0955877f653
tuncaykozak <tuncaykozak@gmail.com>
2022-11-06 13:56:04 +0300

    basic mistakes fixed

--- EH_Chapter-3/src/Q5.java
+++ EH_Chapter-3/src/Q5.java
@@ -9,28 +9,25 @@ public class Q5 {
     public static void test() {
 
         double cost = 1000;
-        double interestRatePerYear = 18;
         double interestRatePerMonth = 1.5;
         double monthlyPayment = 50;
 
         System.out.printf("Payment will take up to %d months!",
-                calculatePaymentPeriod(cost, interestRatePerYear, interestRatePerMonth, monthlyPayment));
+                calculatePaymentPeriod(cost, interestRatePerMonth, monthlyPayment));
 
     }
 
-    public static int calculatePaymentPeriod(double cost, double interestRatePerYear, double interestRatePerMonth, double monthlyPayment) {
+    public static int calculatePaymentPeriod(double cost, double interestRatePerMonth, double monthlyPayment) {
 
         int numberOfMonth = 0;
 
         while (cost > 0) {
 
-            if (numberOfMonth % 12 == 0)
-                cost *= (interestRatePerYear / 100 + 1);
-
             cost *= (interestRatePerMonth / 100 + 1);
             cost -= monthlyPayment;
 
             numberOfMonth++;
+
         }
 
         return numberOfMonth;
