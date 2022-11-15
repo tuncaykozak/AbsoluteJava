26d970f6129cffb6857d5480936507ea220bc9da
tuncaykozak <tuncaykozak@gmail.com>
2022-11-15 19:26:22 +0300

    Updated

--- EH_Chapter-3/src/Q2.java
+++ EH_Chapter-3/src/Q2.java
@@ -6,39 +6,40 @@ public class Q2 {
 
     }
 
-    public static void test (){
+    public static void test() {
 
-        String correctPattern = "RBGBR";
-        String wrongPatternWithWrongChar = "RXGBR";
-        String wrongPattern = "RRGBR";
+        java.util.Scanner kb = new java.util.Scanner(System.in);
 
-        showPattern(correctPattern);
-        showPattern(wrongPatternWithWrongChar);
-        showPattern(wrongPattern);
+        int count = 0;
+        String beforeColor = "";
+        String pattern = "";
 
-    }
+        do {
+            System.out.print("Enter the color of pattern : ");
+            String nextColor = kb.nextLine().substring(0, 1);
 
-    public static void showPattern(String pattern){
-        if(checkPattern(pattern))
-            System.out.println(pattern);
-    }
-
-    public static boolean checkPattern(String pattern){
-
-        for(int i = 0; i < pattern.length(); i++){
-
-            String ch = pattern.substring(i, i + 1);
-
-            if(!ch.equalsIgnoreCase("R")  && !ch.equalsIgnoreCase("G") && !ch.equalsIgnoreCase("B") )
-                return false;
-
-            if(i < pattern.length() -1){
-
-                if(ch.equalsIgnoreCase(pattern.substring(i + 1, i +2)))
-                    return false;
-
+            if (checkPattern(nextColor, beforeColor)) {
+                beforeColor = nextColor;
+                pattern += nextColor.toUpperCase();
+                count++;
             }
+        }
+        while (count < 5);
 
+        System.out.println(pattern);
+
+    }
+
+    public static boolean checkPattern(String currentColor, String beforeColor) {
+
+        if (!currentColor.equalsIgnoreCase("R") && !currentColor.equalsIgnoreCase("G") && !currentColor.equalsIgnoreCase("B")) {
+            System.out.println("You have to enter R / G / B !");
+            return false;
+        }
+
+        if (currentColor.equalsIgnoreCase(beforeColor)) {
+            System.out.println("You can not enter previous color!");
+            return false;
         }
 
         return true;
